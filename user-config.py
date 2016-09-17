12d31834e5a2f410c1f6cc1436e52cc10e9e7711
emijrp <emijrp@gmail.com>
2016-09-17 20:54:05 +0200

    ficheros de configuración

---
+++ user-config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8  -*-
+
+mylang = 'wikanda'
+family = 'wikanda'
+
+usernames['wikanda']['wikanda'] = u'Emijrp'
+usernames['wikanda']['cadizpedia'] = u'Emijrp'
+#sysopnames['wikanda']['cadizpedia'] = u'Emijrp'
+
