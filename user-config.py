e574c81c0ade5d2adf90a5377b602f8c560287e7
emijrp <emijrp@gmail.com>
2016-09-20 14:29:46 +0200

    Wikanda user

--- user-config.py
+++ user-config.py
@@ -4,6 +4,7 @@ mylang = 'wikanda'
 family = 'wikanda'
 
 usernames['wikanda']['wikanda'] = u'Emijrp'
-usernames['wikanda']['cadizpedia'] = u'Emijrp'
+#usernames['wikanda']['cadizpedia'] = u'Emijrp'
+usernames['wikanda']['huelvapedia'] = u'Wikanda'
 #sysopnames['wikanda']['cadizpedia'] = u'Emijrp'
 
