2f9928b6b78d91387678ff8ca063746b500ec5fb
emijrp <emijrp@gmail.com>
2016-09-21 21:19:58 +0200

    almeriapedia

--- user-config.py
+++ user-config.py
@@ -3,8 +3,10 @@
 mylang = 'wikanda'
 family = 'wikanda'
 
-usernames['wikanda']['wikanda'] = u'Emijrp'
+#usernames['wikanda']['wikanda'] = u'Emijrp'
+usernames['wikanda']['wikanda'] = u'Wikanda'
 #usernames['wikanda']['cadizpedia'] = u'Emijrp'
+usernames['wikanda']['almeriapedia'] = u'Wikanda'
 usernames['wikanda']['huelvapedia'] = u'Wikanda'
 #sysopnames['wikanda']['cadizpedia'] = u'Emijrp'
 
